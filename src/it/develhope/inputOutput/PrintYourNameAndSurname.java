2bd52d392868f72973f76b32fffa00dc3b722800
Lorenzo Balestrini <lo.bale@libero.it>
2022-12-07 16:15:05 +0100

    input and output 3

---
+++ src/it/develhope/inputOutput/PrintYourNameAndSurname.java
@@ -0,0 +1,9 @@
+package it.develhope.inputOutput;
+
+public class PrintYourNameAndSurname {
+    public static void main(String[] args) {
+
+        System.out.println("Lorenzo");
+        System.out.println("Balestrini");
+    }
+}
